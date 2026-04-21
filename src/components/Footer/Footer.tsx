a9e456f174794ff029382dc143fc3afed685efe0
Gabrielprograme <jgmarques859@gmail.com>
2026-04-21 18:28:29 -0300

    Configaração

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2024 Gabriel. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Meu Portfólio</h1>
+        </header>
+    )
+}
+
+export default Header;
